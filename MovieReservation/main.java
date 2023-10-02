acc5aa3df2c770e1635e2f58d9faa066d19e89a7
JunCaMor <128831142+JunCaMor@users.noreply.github.com>
2023-10-02 13:28:28 +0800

    Initial MovieReservation main.java file

---
+++ MovieReservation/main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void (String[] args){
+        Movie[] movie=new Movie[]{
+            new Movie("2023-10-02","1","H3,H4,H5","1,500.00")//String, int, String, double
+        };
+    }
+}
