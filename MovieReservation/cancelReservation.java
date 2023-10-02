75d88cc46655abf03d354b7ad81db9b59a2f1193
KibaMaineShar <ry.shar8857@gmail.com>
2023-10-02 14:03:18 +0800

    cancelReservation Testing

---
+++ MovieReservation/cancelReservation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public cancelReservation{
+
+    public void cancelReservation(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\tCancel Reservation Page")
+        System.out.println("Please enter Reservation ID: ");
+
+        String rID = input.nextString();
+
+        
+
+    }
+
+}
