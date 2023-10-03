2fe8447872d9636601e98bf4361c043bf27b8fd5
KibaMaineShar <ry.shar8857@gmail.com>
2023-10-03 15:19:32 +0800

    Implement ParseException in Main.java

--- MovieReservation/Main.java
+++ MovieReservation/Main.java
@@ -1,6 +1,8 @@
+import java.text.ParseException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		ReserveSeat[] reserve=new ReserveSeat[]{
 			new ReserveSeat("2023-10-02","1","12:00","true","Kung Fu Panda","1.75"),
 			new ReserveSeat("2023-10-04","2","12:00","true","Kung Fu Hustle","1.75"),
