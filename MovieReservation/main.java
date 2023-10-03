a83670568c63116792fac2b6c54ec61864b873d7
yatootz23 <amiellabajo05@gmail.com>
2023-10-03 13:23:16 +0800

    Added select transaction

--- MovieReservation/main.java
+++ MovieReservation/main.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		ReserveSeat[] reserve=new ReserveSeat[]{
-			new ReserveSeat("2023-10-02","1","12:00","true","Kung Fu Panda","1.75"),
-			new ReserveSeat("2023-10-04","2","12:00","true","Kung Fu Hustle","1.75"),
-		};
-		System.out.println(reserve[0]);
-		System.out.println(reserve[1]);
+		Scanner scan = new Scanner(System.in);
+		int transaction;
+		System.out.println("Welcome to Cinema World!\n");
+		System.out.println("[1] Reserve Movie Ticket\n[2] Cancel Reservation Ticket\n");
+		System.out.print("Enter your desired transaction: ");
+		transaction = scan.nextInt();
+		if (transaction == 1){
+			System.out.println("Insert ReserveSeat module here");
+		} else if (transaction == 2){
+			System.out.println("Insert CancelReservation module here");
+		}else{
+			System.out.println("Invalid Transaction");
+		}
 	}
 
 }
