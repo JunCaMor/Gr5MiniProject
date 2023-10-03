2624426752dc4695eb3a13d31d60df3c211d3618
JunCaMor <128831142+JunCaMor@users.noreply.github.com>
2023-10-03 16:48:44 +0800

    Update ReserveSeat.java

--- MovieReservation/ReserveSeat.java
+++ MovieReservation/ReserveSeat.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.moviereservation;
-
-import java.util.Scanner;
-
 /**
  *
  * @author junel.mora
  */
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
+import java.util.Scanner;
 public class ReserveSeat {
     private String id="";
     private String date;
