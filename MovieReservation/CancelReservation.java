916f35312ffe60a392e481865f3946ac520a57d0
KibaMaineShar <ry.shar8857@gmail.com>
2023-10-03 15:19:01 +0800

    Implement ParseException

--- MovieReservation/CancelReservation.java
+++ MovieReservation/CancelReservation.java
@@ -4,13 +4,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 public class CancelReservation {
 
-    public void cancel() {
+    public void cancel() throws ParseException {
         boolean response = true;
 
         try (Scanner input = new Scanner(System.in)) {
@@ -33,7 +36,21 @@ public class CancelReservation {
                                 idFound = true; // Mark ID as found
                                 System.out.println("____________________________________________________________");
                                 System.out.println("\nCinema " + parts[2].replace("\"", ""));
-                                System.out.println(parts[1].replace("\"", ""));
+
+                                 String inputDateStr = parts[1].replace("\"", ""); //Change index based on location of date
+
+                                // Create a SimpleDateFormat object for parsing the input date
+                                SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+                                // Parse the input date string into a Date object
+                                Date date = inputDateFormat.parse(inputDateStr);
+
+                                // Create a SimpleDateFormat object for formatting the date in the desired format
+                                SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMMM yyyy");
+
+                                String formattedDate = outputDateFormat.format(date);
+
+                                System.out.println(formattedDate);// Print the formatted date
                                 System.out.println(parts[0].replace("\"", "") + ", @" + parts[3].replace("\"", ""));
                                 System.out.println(parts[4].replace("\"", ""));
                                 System.out.println("\nRef no.: " + parts[0].replace("\"", "") + "\t\tPHP "
@@ -72,11 +89,12 @@ public class CancelReservation {
                 } finally {
                     System.out.print("\nDo you want to cancel another reservation? [Y/N]: ");
                     String anotherReservation = input.nextLine();
-                    if (anotherReservation.equalsIgnoreCase("y")) {
-                        response = true;
-                    } else {
-                        response = false;
-                    }
+                    // if (anotherReservation.equalsIgnoreCase("y")) {
+                    //     response = true;
+                    // } else {
+                    //     response = false;
+                    // }
+                    response = anotherReservation.equalsIgnoreCase("y");
                 }
             }
         }
