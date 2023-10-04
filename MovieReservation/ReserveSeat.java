976b138403d663e0498f3055fd7180bd48800526
yatootz23 <amiellabajo05@gmail.com>
2023-10-04 14:58:58 +0800

    Optimized ReserveSeat

--- MovieReservation/ReserveSeat.java
+++ MovieReservation/ReserveSeat.java
@@ -1,14 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import java.util.Scanner;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
+
 public class ReserveSeat {
     private String id="";
     private String date;
